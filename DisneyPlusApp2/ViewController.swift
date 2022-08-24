dfc051f41781a587d9e5f4a55604dd09620a1424
yolkar62 <mert.murat2@hotmail.com>
2022-08-24 13:22:09 +0300

    Initial Commit

---
+++ DisneyPlusApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DisneyPlusApp2
+//
+//  Created by MERT MURAT on 24.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
